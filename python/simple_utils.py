9448898e73cd4b6a8d9db05f1fff97936795dca0
alisha-123-code <tyahadialisha@gmail.com>
2025-07-18 12:09:28 +0530

    Update simple_utils.py
    Updated the changes made from syntax errors

--- python/simple_utils.py
+++ python/simple_utils.py
@@ -1,9 +1,10 @@
-Python
 # simple_utils.py - A tiny utility library
 def reverse_string(text):
-"""Reverses the characters in a string."""
-return text[::-1]
+  """Reverses the characters in a string."""
+  return text[::-1]
 def count_words(sentence):
-return len(sentence.split())
+  ""Counts the number of words in a sentence."""
+  return len(sentence.split())
 def celsius_to_fahrenheit(celsius):
-return (celsius * 9/5) + 32
+  """Converts Celsius temperature to Fahrenheit."""
+  return (celsius * 9/5) + 32
