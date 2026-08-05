e31cf3e490f1f6434888f372512a2661e1f3454f
dg2013 <davegoodhand1@gmail.com>
2026-08-05 09:53:31 +0100

    Refine header comment in math_utils.py
    Removed redundant comment from the top of the file.

--- python/math_utils.py
+++ python/math_utils.py
@@ -1,4 +1,4 @@
-# math_utils.py - Mathematical utility functions - comment 1
+# math_utils.py - Mathematical utility functions
 def add(a, b): return a + b 
 def multiply(a, b): return a * b 
 def is_even(n): return n % 2 == 0 
